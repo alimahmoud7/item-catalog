c4e9c01e2f5521a03ddbd972d0e1d74f2bcc1405
Ali Mahmoud <ali.mahmoud.engineer@gmail.com>
2017-08-12 00:39:29 +0200

    Add Database tables with ORM SQLAlchemy

---
+++ database_setup.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Field(Base):
+    """Represent a field in Computer Science"""
+    __tablename__ = 'field'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id':   self.id,
+            'name': self.name
+        }
+
+
+class MOOC(Base):
+    """Represent an online course exists in a MOOC provider"""
+    __tablename__ = 'mooc'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    provider = Column(String(80), nullable=False)
+    creator = Column(String(80))
+    level = Column(String(32))
+    description = Column(String(250))
+    cost = Column(String(10))
+    link = Column(String)
+    image = Column(String)
+    field_id = Column(Integer, ForeignKey('field.id'))
+    field = relationship("Field")
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id':       self.id,
+            'title':    self.title,
+            'provider': self.provider,
+            'creator':  self.creator,
+            'level': self.level,
+            'description': self.description,
+            'cost': self.cost,
+            'link': self.link,
+            'image': self.image
+        }
+
+
+engine = create_engine('sqlite:///top_mooc.db')
+
+Base.metadata.create_all(engine)
