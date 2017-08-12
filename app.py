648da410eec17488d4df6eafccf165d09224e957
Ali Mahmoud <ali.mahmoud.engineer@gmail.com>
2017-08-12 14:10:40 +0200

    Add Initial Templates & Forms

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from database_setup import Base, Field, MOOC
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 
 engine = create_engine('sqlite:///top_mooc.db')
+
+# Make a connection between class definitions and the corresponding tables within database
 Base.metadata.bind = engine
+# Establish a link of connection between code execution and the engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
@@ -19,47 +22,65 @@ def index():
     return "All fields with latest moocs here!"
 
 
-@app.route('/fields/new')
+@app.route('/fields/new', methods=['GET', 'POST'])
 def new_field():
     """Add a new CS field"""
-    return "Create a new field here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('new_field.html')
 
 
-@app.route('/fields/<int:field_id>/edit')
+@app.route('/fields/<int:field_id>/edit', methods=['GET', 'POST'])
 def edit_field(field_id):
     """Edit a CS field"""
-    return "Edit a specific field here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('edit_field.html')
 
 
-@app.route('/fields/<int:field_id>/delete')
+@app.route('/fields/<int:field_id>/delete', methods=['GET', 'POST'])
 def delete_field(field_id):
     """Delete a CS field"""
-    return "Delete a specific field here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('delete_field.html')
 
 
 @app.route('/fields/<int:field_id>/')
 @app.route('/fields/<int:field_id>/moocs/')
 def show_moocs(field_id):
     """Show all MOOCs with a specific field"""
-    return "All moocs with a specific field here!"
+    return render_template('moocs.html')
 
 
-@app.route('/fields/<int:field_id>/moocs/new')
+@app.route('/fields/<int:field_id>/moocs/new', methods=['GET', 'POST'])
 def new_mooc(field_id):
     """Add new MOOC"""
-    return "Create a new mooc here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('new_mooc.html')
 
 
-@app.route('/fields/<int:field_id>/moocs/<int:mooc_id>/edit')
+@app.route('/fields/<int:field_id>/moocs/<int:mooc_id>/edit', methods=['GET', 'POST'])
 def edit_mooc(field_id, mooc_id):
     """Edit a MOOC"""
-    return "Edit a specific mooc here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('edit_mooc.html')
 
 
-@app.route('/fields/<int:field_id>/moocs/<int:mooc_id>/delete')
+@app.route('/fields/<int:field_id>/moocs/<int:mooc_id>/delete', methods=['GET', 'POST'])
 def delete_mooc(field_id, mooc_id):
     """Delete a MOOC"""
-    return "Delete a specific mooc here!"
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('delete_mooc.html')
 
 
 if __name__ == '__main__':
