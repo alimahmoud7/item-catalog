603ff4dc8030aea1a527c9beba8279d2e7cf1f52
Ali Mahmoud <ali.mahmoud.engineer@gmail.com>
2017-08-14 13:39:00 +0200

    Add All li in fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,10 @@
 // To make active class work
-$(document).ready(function(){
-  $(".field-list li").each(function(){
-    var href = $(this).find('a').attr('href');
-    if (href === window.location.pathname) {
-      $(this).addClass('active');
-    }
-  });
+$(document).ready(function () {
+    $(".field-list li").each(function () {
+        $(this).removeClass('active');
+        var href = $(this).find('a').attr('href');
+        if (href === window.location.pathname) {
+            $(this).addClass('active');
+        }
+    });
 });
