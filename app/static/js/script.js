9368f8c2eec514d0b3ed7d4abce1674a5aa43d9f
Ali Mahmoud <ali.mahmoud.engineer@gmail.com>
2017-09-06 23:20:18 +0200

    Fixing active class function

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,10 +1,13 @@
 // To make active class work
-$(document).ready(function () {
-    $(".field-list li").each(function () {
+$(function () {
+    $('.field-list li').each(function () {
         $(this).removeClass('active');
         var href = $(this).find('a').attr('href');
         if (href === window.location.pathname) {
             $(this).addClass('active');
         }
+        else if (window.location.pathname === '/') {
+			$('#all-fields').addClass('active');
+		}
     });
 });
